ac3fdbc3dae6db1966a64eb4c57a80e0f3e4a8e5
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-09-02 00:56:54 -0400

    update MySqlDataProvider update ExpressionVisitor namespace

--- src/Providers/MySql/MySqlDataProvider.php
+++ src/Providers/MySql/MySqlDataProvider.php
@@ -12,8 +12,8 @@ namespace DevNet\Entity\Providers\MySql;
 use DevNet\Entity\Storage\IEntityDataProvider;
 use DevNet\Entity\Storage\IEntityPersister;
 use DevNet\System\Database\DbConnection;
-use DevNet\System\Compiler\ExpressionVisitor;
 use DevNet\System\Exceptions\PropertyException;
+use DevNet\System\Compiler\Expressions\ExpressionVisitor;
 
 class MySqlDataProvider implements IEntityDataProvider
 {
