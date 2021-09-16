148de333a23a157858efaae159e44c0a16a3d8fa
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-09-16 09:40:35 -0400

    add RenameColumn operation

---
+++ src/Migration/Operations/RenameColumn.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Entity\Migration\Operations;
+
+class RenameColumn extends Column
+{
+    protected string $Rename;
+
+    public function __construct(string $table, string $name, string $rename)
+    {
+        $this->Table = $table;
+        $this->Name  = $name;
+        $this->Rename  = $rename;
+    }
+
+    public function accept(OperationVisitor $visitor): void
+    {
+        $visitor->visitRenameColumn($this);
+    }
+}
