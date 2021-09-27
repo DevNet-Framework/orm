da86d6e96501c2f7f5c0a2880dfe0387a7bd54d8
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-09-27 11:46:35 -0400

    add DropForeignKey operation

---
+++ src/Migration/Operations/DropForeignKey.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Entity\Migration\Operations;
+
+class DropForeignKey extends ForeignKey
+{
+    public function __construct(string $table, string $constraint)
+    {
+        $this->Table      = $table;
+        $this->Constraint = $constraint;
+    }
+
+    public function accept(OperationVisitor $visitor): void
+    {
+        $visitor->visitDropForeignKey($this);
+    }
+}
