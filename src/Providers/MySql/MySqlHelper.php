1a9c3c9376c40b6a7ec5d97a1411c90b926455c3
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-10-08 00:23:31 -0400

    add MySqlHelper

---
+++ src/Providers/MySql/MySqlHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Entity\Providers\MySql;
+
+use DevNet\Entity\Storage\ISqlGenerationHelper;
+
+class MySqlHelper implements ISqlGenerationHelper
+{
+    public function delimitIdentifier(string $name, ?string $schema = null): string
+    {
+        if ($schema) {
+            return "`{$schema}`.`{$name}`";
+        }
+
+        return "`{$name}`";
+    }
+}
