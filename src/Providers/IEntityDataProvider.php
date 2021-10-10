fff9b8d4e224d6e46f24393bc9cd9df54080250b
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-10-10 13:38:18 -0400

    update IEntityDataProvider

--- src/Providers/IEntityDataProvider.php
+++ src/Providers/IEntityDataProvider.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types = 1);
+<?php
+
 /**
  * @author      Mohammed Moussaoui
  * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
@@ -10,18 +11,18 @@ namespace DevNet\Entity\Providers;
 
 use DevNet\Entity\Storage\ISqlGenerationHelper;
 use DevNet\Entity\Migration\Operations\OperationVisitor;
-use DevNet\System\Compiler\ExpressionVisitor;
+use DevNet\System\Compiler\Expressions\ExpressionVisitor;
 use DevNet\System\Database\DbConnection;
 use DevNet\System\Exceptions\PropertyException;
 
 interface IEntityDataProvider
-{   
+{
     /**
      * This method must retun the following properties.
      * @return DbConnection $Connection
      * @return ISqlGenerationHelper $SqlHelper
      * @return ExpressionVisitor $QueryGenerator
-     * @return OperationVisitor $SchemaGenerator
+     * @return OperationVisitor $MigrationGenerator
      * and must throw an exception if the property doesn't exist
      * @throws PropertyException
      */
