2c5fb05821c0f3f948a363db4dfeb71d977708df
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-10-15 00:36:54 -0400

    update Operation update the methods createTable, alterTable, renameTable and dropTable add the methods insertData, updateData and deleteData

--- src/Migration/Operations/Operation.php
+++ src/Migration/Operations/Operation.php
@@ -13,23 +13,38 @@ abstract class Operation
 {
     abstract public function accept(OperationVisitor $expressionVisitor): void;
 
-    public static function createTable(string $name, ?string $schema = null): CreateTable
+    public static function createTable(?string $schema, string $name): CreateTable
     {
-        return new CreateTable($name, $schema);
+        return new CreateTable($schema, $name);
     }
 
-    public static function alterTable(string $name, ?string $schema = null): AlterTable
+    public static function alterTable(?string $schema, string $name): AlterTable
     {
-        return new AlterTable($name, $schema);
+        return new AlterTable($schema, $name);
     }
 
-    public static function renameTable(string $name, string $rename, ?string $schema = null): renameTable
+    public static function renameTable(?string $schema, string $name, string $rename): RenameTable
     {
-        return new renameTable($name, $rename, $schema);
+        return new RenameTable($schema, $name, $rename);
     }
 
-    public static function dropTable(string $name, ?string $schema = null): dropTable
+    public static function dropTable(?string $schema, string $name): DropTable
     {
-        return new dropTable($name, $schema);
+        return new DropTable($schema, $name);
+    }
+
+    public static function insertData(?string $schema, string $name, array $columns): InsertData
+    {
+        return new InsertData($schema, $name, $columns);
+    }
+
+    public static function updateData(?string $schema, string $name, array $columns, array $keys): UpdateData
+    {
+        return new UpdateData($schema, $name, $columns, $keys);
+    }
+
+    public static function deleteData(?string $schema, string $name, array $keys): DeleteData
+    {
+        return new DeleteData($schema, $name, $keys);
     }
 }
