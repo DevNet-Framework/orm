3aff42de247ca41403e50c4bade2eed9d478c220
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-10-15 17:51:00 -0400

    update global.php register MigrationGeneratorProvider

--- global.php
+++ global.php
@@ -6,3 +6,8 @@
  * @license     MIT License. For full license information see LICENSE file in the project root.
  * @link        https://github.com/DevNet-Framework
  */
+
+use DevNet\Cli\Templating\CodeGeneratorRegistry;
+use DevNet\Entity\Tools\MigrationGeneratorProvider;
+
+CodeGeneratorRegistry::register('migration', MigrationGeneratorProvider::class);
