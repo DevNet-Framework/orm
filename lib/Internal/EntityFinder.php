68de7a214195d1b26c337c1eebeabb4960704643
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-06-23 14:13:17 -0400

    modify EntityFinder - change the type of the 2nd param in the method find(). - chnage the work logic the handle the value keys.

--- lib/Internal/EntityFinder.php
+++ lib/Internal/EntityFinder.php
@@ -28,16 +28,31 @@ class EntityFinder
         $this->entityStateManager = $database->EntityStateManager;
     }
 
-    public function find(EntityType $entityType, string $keyValue): ?object
+    public function find(EntityType $entityType, array $keyValues): ?object
     {
-        $entry = $this->entityStateManager->getEntry($entityType->getName(), $keyValue);
+        $entry = $this->entityStateManager->getEntry($entityType->Name, $keyValues);
         if ($entry) {
             return $entry->Entity;
         }
 
+        $entity = null;
         $query  = new EntityQuery($entityType, $this->database->QueryProvider);
-        $key    = $entityType->getPrimaryKey();
-        $entity = $query->where(fn ($entity) => $entity->$key == $keyValue)->first();
+        if (count($keyValues) > 0 && count($keyValues) == count($entityType->Keys)) {
+            switch (count($keyValues)) {
+                case 1:
+                    $key = $entityType->Keys[0];
+                    $keyValue = $keyValues[0];
+                    $entity = $query->where(fn ($entity) => $entity->$key == $keyValue)->first();
+                    break;
+                case 2:
+                    $key1 = $entityType->Keys[0];
+                    $key2 = $entityType->Keys[1];
+                    $keyValue1 = $keyValues[0];
+                    $keyValue2 = $keyValues[1];
+                    $entity = $query->where(fn ($entity) => $entity->$key1 == $keyValue1 && $entity->$key2 == $keyValue2)->first();
+                    break;
+            }
+        }
 
         if ($entity) {
             $this->load($entity);
@@ -53,29 +68,31 @@ class EntityFinder
         $entityType = $this->database->Model->getEntityType(get_class($entity));
         foreach ($entityType->Navigations as $navigation) {
             $navigation->PropertyInfo->setAccessible(true);
-            if ($navigation->NavigationType == 2) {
-                $key = $entityType->getPrimaryKey();
+            if ($navigation->Cardinality == 2 && count($entityType->Keys) == 1) {
+                $key = $entityType->Keys[0];
                 $navigation->PropertyInfo->setValue($entity, new EntityCollection($navigation, $this->database, $entity->$key));
-            } else if ($navigation->NavigationType == 1) {
-                $foreignKey = $navigation->Metadata->getForeignKey($navigation->MetadataReference->getName());
+            } else if ($navigation->Cardinality == 1) {
+                $foreignKey = $navigation->EntityType->getForeignKey($navigation->TargetEntityType->Name);
                 if ($foreignKey) {
-                    $ParentEntity = $this->find($navigation->MetadataReference, $entity->$foreignKey);
+                    $ParentEntity = $this->find($navigation->TargetEntityType, [$entity->$foreignKey]);
                     $navigation->PropertyInfo->setValue($entity, $ParentEntity);
                 } else {
-                    $key = $entityType->getPrimaryKey();
-                    $childEntity = $this->query($navigation, $entity->$key)->first();
-                    if ($childEntity) {
-                        $navigation->PropertyInfo->setValue($entity, $childEntity);
+                    if (count($entityType->Keys) == 1) {
+                        $key = $entityType->Keys[0];
+                        $childEntity = $this->query($navigation, $entity->$key)->first();
+                        if ($childEntity) {
+                            $navigation->PropertyInfo->setValue($entity, $childEntity);
+                        }
                     }
                 }
             }
         }
     }
 
-    public function query(EntityNavigation $navigation, $keyValue): IQueryable
+    public function query(EntityNavigation $navigation, string $keyValue): IQueryable
     {
-        $query = new EntityQuery($navigation->MetadataReference, $this->database->QueryProvider);
-        $foreignKey = $navigation->getForeignKey();
+        $query = new EntityQuery($navigation->TargetEntityType, $this->database->QueryProvider);
+        $foreignKey = $navigation->TargetEntityType->getForeignKey($navigation->EntityType->Name);
 
         return $query->where(fn ($entity) => $entity->$foreignKey == $keyValue);
     }
