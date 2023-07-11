cb6ab002c79588d6874b2be3252002b2a00ee162
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-07-11 15:56:01 -0400

    rename DropPrimaryKey operation - rename it to DropPrimaryKeyOperation

--- lib/Migration/Operations/DropPrimaryKeyOperation.php
+++ lib/Migration/Operations/DropPrimaryKeyOperation.php
@@ -9,7 +9,7 @@
 
 namespace DevNet\Entity\Migration\Operations;
 
-class DropPrimaryKey extends PrimaryKey
+class DropPrimaryKeyOperation extends PrimaryKeyOperation
 {
     public function accept(OperationVisitor $visitor): void
     {
