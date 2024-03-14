60d1a1010cfa702f4e337cbce242887f19d18d8a
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2024-03-14 00:54:32 -0400

    update the use of Plugin namespace to match the last change of DevNet CLI

--- global.php
+++ global.php
@@ -6,16 +6,16 @@
  * @link        https://github.com/DevNet-Framework
  */
 
-use DevNet\CLI\Commands\CommandRegistry;
 use DevNet\CLI\Plugin\CodeGeneratorRegistry;
+use DevNet\CLI\Plugin\CommandRegistry;
 use DevNet\ORM\Tools\MigrateCommand;
 use DevNet\ORM\Tools\MigrationGeneratorProvider;
 
 /**
- * DevNet CLI package is not mandatory required by DevNet Entity package,
+ * DevNet CLI package is not mandatory required by DevNet ORM package,
  * so we need to check first if the DevNet CLI is installed before registering the command.
  */
 if (class_exists(CommandRegistry::class)) {
-    CommandRegistry::register('migrate', MigrateCommand::class);
+    CommandRegistry::register('migrate',MigrateCommand::class);
     CodeGeneratorRegistry::register('migration', MigrationGeneratorProvider::class);
 }
